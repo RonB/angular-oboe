da0b1ca2f62c37d0b5ab3b448d1e61d2fad3b66f
Ronald Brinkerink <ronb.brein@gmail.com>
2014-09-06 09:30:37 +0200

    changed default node to any

--- src/angular-oboe.js
+++ src/angular-oboe.js
@@ -4,7 +4,7 @@ angular.module('ngOboe', [])
             // the passed parameters object need to have a Url and a pattern.
             // all parameters consumed by the oboe module can be consumed
             // the url needs to return a json stream. see the oboe documentation
-            // the patterns contains a path which selects json objects from the stream
+            // the pattern contains a path which selects json objects from the stream
             // the pagesize parameter determines how many JSON objects need to be received before the controller gets updated. Defaults to 100.
             return function(params) {
                 // initialize the data
@@ -39,8 +39,8 @@ angular.module('ngOboe', [])
                                 .start(function() {
                                     defer.resolve();
                                 })
-                                // for every node containing the specified pattern
-                                .node(params.pattern || '{_id}', function(node) {
+                                // for every node containing the specified pattern or if not specified any node
+                                .node(params.pattern || '.', function(node) {
                                     //  we push the node to the page
                                     page.push(node);
                                     counter++;
