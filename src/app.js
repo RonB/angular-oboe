cb62e37e1c0022f86ef71c0ccbcc3fe36a05ff7c
Ronald Brinkerink <ronb.brein@gmail.com>
2015-03-15 20:22:33 +0100

    return promise instead of data to give developer more control

--- src/app.js
+++ src/app.js
@@ -20,19 +20,27 @@ angular.module('Contacts', ['ngOboe', 'ui.router', 'sf.virtualScroll'])
         .controller('StreamingCtrl', function($scope, Oboe) {
             $scope.offset = 0; // starting offset for virtual-scroll
             $scope.contacts = [];
-            // the contacts streamed
-            $scope.contacts = Oboe({
+            $scope.status = 'reading....';
+            Oboe({
                 url: 'http://www.webbergen.nl/data/publicatie-website/largejson/contacts2.json',
-                pattern: '{index}', // all nodes that have an index property will be included
-                pagesize: 100
+                pattern: '{index}' // all nodes that have an index property will be included
+            })
+            .then(function() {
+               $scope.status = 'done';
+            }, function(error) {
+                // some error
+            }, function(record) {
+               $scope.contacts.push(record);
             });
         })
         .controller('NoStreamingCtrl', function($scope, $http) {
             $scope.offset = 0; // starting offset for virtual-scroll
             // the contacts read entirely before added to the scope
             $scope.contacts = [];
+            $scope.status = 'reading....';
             $http.get('http://www.webbergen.nl/data/publicatie-website/largejson/contacts2.json')
                     .success(function(response) {
                         $scope.contacts = response;
+                        $scope.status = 'done';
                     });
         });
